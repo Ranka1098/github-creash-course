26b08333952741cdbf2fb0c81e717aff12f798eb
ashok Ranka <ashokranka30@gmail..com>
2024-02-13 22:50:46 +0530

    remove above18 function

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const age=24;
 const name = " ashok";
 const dob= "10/04/1997"
 
-const sayname=()=>{
+const sayName=()=>{
 console.log("name" + name);
 console.log("name" + name1);
-}
-
-function isAbove18(){
-return age>=18 ;
 }
