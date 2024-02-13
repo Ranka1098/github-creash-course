63ca1d3064b8b67f2c4359bd3d35010ce189820d
ashok Ranka <ashokranka30@gmail..com>
2024-02-13 23:34:57 +0530

    Revert "remove above18 function"
    This reverts commit 26b08333952741cdbf2fb0c81e717aff12f798eb.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const age=24;
 const name = " ashok";
 const dob= "10/04/1997"
 
-const sayName=()=>{
+const sayname=()=>{
 console.log("name" + name);
 console.log("name" + name1);
+}
+
+function isAbove18(){
+return age>=18 ;
 }
